6bbaca86b9f6d478fd7bda136971353c8a52d107
Skok-off <skokff@yandex.ru>
2024-11-28 01:00:12 +0500

    Диплом. Этап 2. Основной сервис

--- stat/service/src/main/java/ru/practicum/service/HitService.java
+++ stat/service/src/main/java/ru/practicum/service/HitService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 public interface HitService {
+
     void addHit(HitDto hitDto);
 
     List<HitStatDto> getStats(LocalDateTime start, LocalDateTime end, Set<String> uris, Boolean unique);
